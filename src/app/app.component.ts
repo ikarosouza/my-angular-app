5630b1eb38b98ea776e7c5f0d27dbe5d80925245
Ikaro Bezerra De Souza (BFSI - Brazil) <IK20004302@wipro.com>
2018-08-01 14:40:58 -0300

    if else examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent {
   months = ["January", "Feburary", "March", "April",
             "May", "June", "July", "August", "September",
             "October", "November", "December"];
+  isAvailable = false;
 }
