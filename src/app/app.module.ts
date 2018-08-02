b92f2ce5be61539dc61132ab6b4d984338ac9fbd
Ikaro Souza <IK20004302@wipro.com>
2018-08-02 14:48:34 -0300

    Directive and pipes examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ChangeTextDirective } from './change-text.directive';
+import { SqrtPipe } from './sqrt.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ChangeTextDirective,
+    SqrtPipe
   ],
   imports: [
     BrowserModule
